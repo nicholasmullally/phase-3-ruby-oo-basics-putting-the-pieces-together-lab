e16f80759ce4d3ed2b0f2393eb7c9b20920afcf1
Nicholas <mullally.nt@gmail.com>
2022-07-31 16:55:37 -0700

    Done with assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
+class Book
+    def initialize(title, author, pages, genre)
+        @title = title
+        @author = author_name
+        @pages = page_count
+        @genre = genre
+    end
+
+    def getTitle()
+        @title
+    end
+
+    def getAuthor()
+        @author
+    end
+
+    def getPages()
+        @pages
+    end
+
+    def getGenre()
+        @genre
+    end
+end
+
+
 
 
